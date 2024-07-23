b70ac0c6e9bc97c91d434afa23fe6b51cc5ebe38
AnKub <ankub3088@gmail.com>
2024-07-23 20:31:42 +0300

    endbackrefact

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import multer from 'multer';
-import {registerValidation, loginValidation, postCreateValidation} from './validations.js';
-import checkAuth from "./utils/checkAuth.js";
 
-import * as UserController from './controllers/UserController.js';
-import * as PostController from './controllers/PostController.js';
-import handleValidationErrors from './utils/handleValidationErrors.js';
+import {registerValidation, loginValidation, postCreateValidation} from './validations.js';
+import {checkAuth, handleValidationErrors} from "./utils/index.js";
+import {UserController, PostController} from './controllers/index.js';
 
 
 mongoose
@@ -39,8 +37,8 @@ const upload = multer({storage});
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
-app.post('/auth/login', handleValidationErrors, loginValidation, UserController.login);
-app.post('/auth/register', handleValidationErrors, registerValidation, UserController.register );
+app.post('/auth/login',  loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors,  UserController.register );
 app.get('/auth/me', checkAuth, UserController.getMe );
 
 // download schem
@@ -54,9 +52,9 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 
 app.get('/posts', PostController.getAll );
 app.get('/posts/:id', PostController.getOne);
-app.post('/posts',checkAuth, postCreateValidation, PostController.create);
+app.post('/posts',checkAuth, postCreateValidation,handleValidationErrors, PostController.create);
 app.delete('/posts/:id',checkAuth, PostController.remove);
-app.patch('/posts/:id',checkAuth, postCreateValidation, PostController.update);
+app.patch('/posts/:id',checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
 
 
 app.listen(4444, (err) => {
