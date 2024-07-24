84c7dc0c54e4e9267c774b5043ae36641f132a5e
AnKub <ankub3088@gmail.com>
2024-07-24 21:58:58 +0300

    getTags

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 
 // crud for posts
 
+app.get('/tags', PostController.getLastTags);
+
 app.get('/posts', PostController.getAll );
 app.get('/posts/tags', PostController.getLastTags);
 app.get('/posts/:id', PostController.getOne);
