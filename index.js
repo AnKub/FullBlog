bdc9e3a64be69071bd5d567fed04f605618e8444
AnKub <ankub3088@gmail.com>
2024-07-24 19:02:59 +0300

    addcors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import mongoose from 'mongoose';
 import multer from 'multer';
 
@@ -35,6 +36,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login',  loginValidation, handleValidationErrors, UserController.login);
