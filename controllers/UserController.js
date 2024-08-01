b4e90b1dfd8d0667804008641d2d02edbb904faa
AnKub <ankub3088@gmail.com>
2024-08-01 11:26:15 +0300

    check

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,7 +2,43 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import UserModel from '../models/User.js';
 
+export const register = async (req, res) => {
+  try {
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
+    const doc = new UserModel({
+      fullName: req.body.fullName,
+      email: req.body.email,
+      passwordHash: hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Registration failed',
+    });
+  }
+};
 
 export const login = async (req, res) => {
   try {
