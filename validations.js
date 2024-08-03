071a9189d28e24e8c408db6daa53a13f3cc534b2
AnKub <ankub3088@gmail.com>
2024-08-03 11:19:49 +0300

    fix

--- validations.js
+++ validations.js
@@ -6,3 +6,14 @@ export const registerValidation = [
   body('fullName', 'Enter your full name').isLength({ min: 3 }),
 ];
 
+export const loginValidation = [
+  body('email', 'Invalid email format').isEmail(),
+  body('password', 'Password must be at least 5 characters long').isLength({ min: 4 }),
+];
+
+export const postCreateValidation = [
+  body('title', 'Enter title of the post').isLength({ min: 3 }).isString(),
+  body('text', 'Enter text of the post').isLength({ min: 10 }).isString(),
+  body('tags', 'Invalid tags format (should be an array)').optional().isArray(),
+  body('imageUrl', 'Invalid image link').optional().isString(),
+];
