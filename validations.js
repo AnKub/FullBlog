81e275d306b41d250b82a53d691dcce151170a94
AnKub <ankub3088@gmail.com>
2024-08-06 22:00:26 +0300

    fix

--- validations.js
+++ validations.js
@@ -2,18 +2,23 @@ import { body } from 'express-validator';
 
 export const registerValidation = [
   body('email', 'Invalid email format').isEmail(),
-  body('password', 'Password must be at least 5 characters long').isLength({ min: 4 }),
-  body('fullName', 'Enter your full name').isLength({ min: 3 }),
+  body('password', 'Password must be at least 6 characters long').isLength({ min: 6 }),
+  body('fullName', 'Full name must be at least 3 characters long and contain only letters and spaces').isLength({ min: 3 }).matches(/^[a-zA-Z\s]+$/),
 ];
 
 export const loginValidation = [
   body('email', 'Invalid email format').isEmail(),
-  body('password', 'Password must be at least 5 characters long').isLength({ min: 4 }),
+  body('password', 'Password must be at least 6 characters long').isLength({ min: 6 }),
 ];
 
 export const postCreateValidation = [
-  body('title', 'Enter title of the post').isLength({ min: 3 }).isString(),
-  body('text', 'Enter text of the post').isLength({ min: 10 }).isString(),
-  body('tags', 'Invalid tags format (should be an array)').optional().isArray(),
-  body('imageUrl', 'Invalid image link').optional().isString(),
+  body('title', 'Title must be at least 3 characters long').isLength({ min: 3 }).isString(),
+  body('text', 'Text must be at least 10 characters long').isLength({ min: 10 }).isString(),
+  body('tags', 'Tags should be an array of strings').optional().isArray().custom(tags => {
+    if (tags.some(tag => typeof tag !== 'string')) {
+      throw new Error('Each tag should be a string');
+    }
+    return true;
+  }),
+  body('imageUrl', 'Invalid image URL').optional().isURL(),
 ];
